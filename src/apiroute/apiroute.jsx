ff41051028e56b39db0488e53230758e4edb8493
ranjith20112-crypto <ranjith20112@gmail.com>
2026-09-10 17:54:32 +0530

    Update apiroute.jsx

--- src/apiroute/apiroute.jsx
+++ src/apiroute/apiroute.jsx
@@ -1,7 +1,7 @@
 // src/apiroute/apiroute.jsx
 import axios from 'axios';
 
-const API_BASE_URL = 'https://backend-r6xl.onrender.com/api'; // Change this to your backend URL in production
+const API_BASE_URL = 'https://backend-r6xl.onrender.com'; // Change this to your backend URL in production
 
 const api = axios.create({
   baseURL: API_BASE_URL,
